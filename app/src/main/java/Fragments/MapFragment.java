0dafd7e8aa0493a41fce67c195aa99957985fac6
Arvi-H <arvihaxhillari1@gmail.com>
2023-04-18 16:20:28 -0600

    stable ubroken app version markers color cordinated

--- app/src/main/java/Fragments/MapFragment.java
+++ app/src/main/java/Fragments/MapFragment.java
@@ -2,6 +2,7 @@ package Fragments;
 
 import static com.google.android.gms.maps.CameraUpdateFactory.newLatLng;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,23 +11,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.family_map_client.DataCache;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.family_map_client.R;
 
+import java.util.HashMap;
 import java.util.Map;
-
 import Model.Event;
-
 public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapLoadedCallback {
     private GoogleMap map;
     private Map<String, Event> events;
     private final DataCache dataCache = DataCache.getInstance();
+    private Map<Marker, Event> mapOfMarkers = new HashMap<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,13 +61,41 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
     }
 
     @Override
-    public void onMapLoaded() {}
+    public void onMapLoaded() {
+    }
 
-    private void addMarkers(){
-        for(Event currEvent : events.values()){
+    private void addMarkers() {
+        // Define a HashMap to store the event types and their corresponding colors
+        Map<String, Float> mapOfColors = new HashMap<>();
+
+        // Predefine colors for known event types
+        mapOfColors.put("birth", BitmapDescriptorFactory.HUE_RED);
+        mapOfColors.put("death", BitmapDescriptorFactory.HUE_BLUE);
+        mapOfColors.put("marriage", BitmapDescriptorFactory.HUE_GREEN);
+
+        // Define the initial marker color value
+        float markerColorValue = 50.0f;
+
+        // Loop through our events
+        for (Event currEvent : events.values()) {
+            String eventType = currEvent.getEventType().toLowerCase();
+            float color;
+
+            if (mapOfColors.containsKey(eventType)) {
+                color = mapOfColors.get(eventType);
+            } else {
+                color = markerColorValue;
+                mapOfColors.put(eventType, color);
+                markerColorValue += 50;
+            }
+
             LatLng newMark = new LatLng(currEvent.getLatitude(), currEvent.getLongitude());
-            map.addMarker(new MarkerOptions().position(newMark).title(currEvent.getEventType()));
-            map.animateCamera(newLatLng(newMark));
+            Marker newM = map.addMarker(new MarkerOptions()
+                    .position(newMark)
+                    .icon(BitmapDescriptorFactory.defaultMarker(color))
+                    .title(currEvent.getEventType()));
+            map.animateCamera(CameraUpdateFactory.newLatLng(newMark));
+            mapOfMarkers.put(newM, currEvent);
         }
     }
 }
